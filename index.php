705881b2e53378e4b79f1e62acb2fa0d24611b88
nicobesn <besniernicolas1310@gmail.com>
2023-01-03 10:25:10 +0100

    Premier fichier : index.php
    Explication création fichier PHP + premier affichage dans le DOM.

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+// Un fichier va toujours 'écrire sans espace, et sans caractères spéciaux
+
+// Un dossier PHP se termine par .php (en minuscule)
+
+// Quand je le trouve dans un dossier, le serveur cherche à éxectuer un fichier index.php
+//  => www.monsite.com.dossier
+// (Pas besoin de préciser index.php)
+
+// Sinon, il faut préciser le nom du fichier
+// => www.monsite.com/dossier/fichier1.php 
+
+// L'intérieur d'un fichier PHP commence toujours par 
+// <?php
+// Plus d'obligation de fermer la balise, avec
+/* ?> */
+
+
+echo 'Mon premier fichier PHP Super !'; 
+
+// Toujours terminer par le ; 
+
+?>
+
+<!-- La je vais envoyer ce code directement au Client  -->
+<!-- Ce code n'est pas executé -->
+
+<h1>Mon premier fichier PHP Super !</h1>; 
