d9b61f3c5b8eef5e2aead1ae5b6d54b147b79ef2
nicobesn <besniernicolas1310@gmail.com>
2023-01-03 11:54:33 +0100

    Nouvelle variables : ENTIERS
    Calcul en PHP

--- variables.php
+++ variables.php
@@ -59,4 +59,81 @@ $autrePhrase .= $piece;
 echo $sautDeLigne;
 echo $sautDeLigne;
 echo $autrePhrase;
+echo $sautDeLigne;
+
+
+// -------------------------------------------------------------------------------------------
+
+// Deuxieme type de variables, les nombres ENTIERS
+
+$unNombre = 35;
+$unAutreNombre = 11;
+
+// Il y a une limite à ce que peut contenir PHP (PHP_INT_MAX)
+// PHP n'est pas "très bon" pour gérer des grands nombres
+
+$unNombreNegatif = -7;
+
+// Calculs
+// 5 opérateurs important pour les calculs
+// Addition : +
+// Soustraction : -
+// Multipication : * 
+// Division : /
+// Modulu : % (reste de la division euclidienne)
+
+$calcul = $unNombre + 9;
+
+$calcul = $unAutreNombre - 34;
+
+$calcul = $unNombre * $unAutreNombre;
+
+$calcul = $unAutreNombre / $unNombre;
+
+$calcul = $unNombre + $unNombre + $unNombre + $unAutreNombre;
+
+echo $sautDeLigne;
+$calcul = $unNombre + ($unAutreNombre * $unNombreNegatif);
+echo $calcul;
+echo $sautDeLigne;
+// On peut faire des opérations mathématiques aussi complexes que nécessaires
+
+$calculIntermédiaire = $unAutreNombre * $unNombreNegatif;
+$calculFinal = $unNombre +$calculIntermédiaire;
+
+$participants = 20;
+$tailleDeGroupe = 3;
+
+
+
+$participantsSansGroupe = $participants % $tailleDeGroupe;
+
+$nbGroupesComplets = ($participants - $participantsSansGroupe) / $tailleDeGroupe;
+
+echo $sautDeLigne;
+echo $nbGroupesComplets;
+
+// Opérateurs condensés
+// Addition : +=
+// Soustraction : -=
+// Multipication : *=
+// Division : /=
+// Modulo : %=
+
+$somme = 0;
+$somme = $somme + 1;
+$somme = $somme + 2;
+$somme = $somme + 3;
+
+// Version condensée
+
+$somme = 0;
+$somme += 1;
+$somme += 2;
+$somme += 3;
+
+// += et -= sont souvent utilisée
+// Les autres beaucoup moins
+// C'est à l'appréciation du développeur 
+
 
